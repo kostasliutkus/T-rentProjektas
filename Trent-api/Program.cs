1b10c293ee5aa28257f965ef06b0abd6de370d5c
Kostas Liutkus <kostliutk2@gmail.com>
2023-11-03 18:02:55 +0200

    changed .gitignore

--- Trent-api/Program.cs
+++ Trent-api/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 //add cors policy
 builder.Services.AddCors(opt =>
