db491e1de997c27a288b8b6e0d630c0748ab5284
Kostas Liutkus <kostliutk2@gmail.com>
2023-12-09 15:53:45 +0200

    login register components created

--- source/Trent-web/src/app/app.module.ts
+++ source/Trent-web/src/app/app.module.ts
@@ -7,15 +7,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { RenterListComponent } from './components/renter-list/renter-list.component';
 
 import { ApiService } from './services/api.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
-    RenterListComponent
+    RenterListComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [ApiService],
   bootstrap: [AppComponent]
