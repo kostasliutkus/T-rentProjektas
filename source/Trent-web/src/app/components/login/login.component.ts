0376ce115afc9aa09128ff9bade1bdf06151004c
Kostas Liutkus <kostliutk2@gmail.com>
2023-12-11 23:22:03 +0200

    register method

--- source/Trent-web/src/app/components/login/login.component.ts
+++ source/Trent-web/src/app/components/login/login.component.ts
@@ -1,40 +1,79 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {Router} from "@angular/router";
 import {UserDto} from "../../models/User.Dto";
 import {ApiLoginService} from "../../services/api.login.service";
 import {SuccessfulLoginDto} from "../../models/SuccessfulLogin.Dto";
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm!: FormGroup;
   userName: string = '';
   password: string = '';
+  showPassword = false;
   constructor(
     private apiLoginService: ApiLoginService,
     private snackBar: MatSnackBar,
-    private router: Router
-  ){}
-  loginUser(): void {
-    const LoginDto = {
-      userName: this.userName,
-      password: this.password,
-    };
-    this.apiLoginService.login(LoginDto).subscribe(
-      (SuccessfulLoginDto: SuccessfulLoginDto) => {
-        console.log('User registered successfully:', SuccessfulLoginDto);
-        this.snackBar.open('login successful', 'Dismiss',{duration: 3000});
-        this.router.navigate(['/']);
-      },
-      (error) => {
-        console.error('Error registering user:', error);
-
-        const errorMessage = error.error?.message || 'An error occurred during registration';
-        this.snackBar.open(errorMessage, 'Error', { duration: 5000 });
-      }
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ){
+    this.matIconRegistry.addSvgIcon(
+      'eye',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/eye.svg')
     );
+    this.matIconRegistry.addSvgIcon(
+      'eye_off',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/eye_off.svg')
+    );
+  }
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      userName: ['',Validators.required],
+      password: ['',Validators.required]
+    })
+  }
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+  navigateToRegister(): void {
+    this.router.navigate(['/register']);
+  }
+  loginUser(): void {
+    if (this.loginForm.valid) {
+      const loginDto = this.loginForm.value;
+
+      this.apiLoginService.login(loginDto).subscribe(
+        (successfulLoginDto: SuccessfulLoginDto) => {
+          console.log('User logged in successfully:', successfulLoginDto);
+
+          // save access Token
+          localStorage.setItem('accessToken', successfulLoginDto.accessToken);
+
+          this.snackBar.open('Login successful', 'Dismiss', { duration: 3000,panelClass:['error-snackbar'] });
+
+          // Reset  form
+          this.loginForm.reset();
+
+          this.router.navigate(['/']);
+        },
+        (error) => {
+          console.error('Error logging in user:', error);
+          // Reset  form
+          this.loginForm.reset();
+          const errorMessage = error.error?.message || 'Password and Username combination does not exist';
+          this.snackBar.open(errorMessage, 'Error', { duration: 5000,panelClass:['error-snackbar'] });
+        }
+      );
+    }
   }
 }
--- source/Trent-web/src/app/components/register/register.component.ts
+++ source/Trent-web/src/app/components/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ApiRegisterService } from '../../services/api.register.service'
 import { RegisterUserDto } from '../../models/Register.User.Dto';
@@ -10,7 +10,8 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
   userName: string = '';
   email: string = '';
   password: string = '';
@@ -18,31 +19,46 @@ export class RegisterComponent {
   constructor(
     private apiRegisterService: ApiRegisterService,
     private snackBar: MatSnackBar,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ){}
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      userName: ['', Validators.required],
+      email: ['',Validators.required],
+      password: ['', Validators.required],
+      passwordConfirmation: ['', Validators.required],
+    })
+  }
   registerUser(): void {
-    if (this.password !== this.passwordConfirmation) {
-      console.error('Passwords do not match');
-      this.snackBar.open('Passwords do not match','Dismiss',{duration:3000});
-      return;
-    }
-    const registerUserDto = {
-      userName: this.userName,
-      email: this.email,
-      password: this.password,
-    };
-    this.apiRegisterService.register(registerUserDto).subscribe(
-      (userDto: UserDto) => {
-        console.log('User registered successfully:', userDto);
-        this.snackBar.open('Registration successful', 'Dismiss',{duration: 3000});
-        this.router.navigate(['/login']);
-      },
-      (error) => {
-        console.error('Error registering user:', error);
-
-        const errorMessage = error.error?.message || 'An error occurred during registration';
-        this.snackBar.open(errorMessage, 'Error', { duration: 5000 });
+    if (this.registerForm.valid) {
+      if (this.password !== this.passwordConfirmation) {
+        console.error('Passwords do not match');
+        this.snackBar.open('Passwords do not match', 'Dismiss', {duration: 3000});
+        return;
       }
-    );
+
+      const registerUserDto = {
+        userName: this.userName,
+        email: this.email,
+        password: this.password,
+      };
+      this.apiRegisterService.register(registerUserDto).subscribe(
+        (userDto: UserDto) => {
+          console.log('User registered successfully:', userDto);
+          this.snackBar.open('Registration successful', 'Dismiss', {duration: 3000});
+          // Reset  form
+          this.registerForm.reset();
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          console.error('Error registering user:', error);
+          // Reset  form
+          this.registerForm.reset();
+          const errorMessage = error.error?.message || 'An error occurred during registration';
+          this.snackBar.open(errorMessage, 'Error', {duration: 5000});
+        }
+      );
+    }
   }
 }
